d6b27a320862056853836c7d03391376daa3995d
bernardtchamou <7568@holbertonstudents.com>
2023-10-13 03:02:04 -0700

    tache 4.1

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
-/* fichier 4 : 4-puts.c */
-int main(void)
+/**
+* main - fonction pricipale
+* puts - commande affichant le message
+* Return: retourner 0 si le code n'a pas d'erreur
+*/
 
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
