6ec208bc6d550db9f39e36f56cd4ff5e6e847d46
bernardtchamou <7568@holbertonstudents.com>
2023-10-13 03:27:27 -0700

    tache 6.1

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 
-/* fichier 6 : 6-size.c */
+/**
+* main - fonction pricipale
+* printf - commande affichant le message
+* Sizeof - indique la taille d'un type d'entier
+* Return: retourner 0 si le code n'a pas d'erreur
+*/
+
 int main(void)
+
 {
-	printf("Size of a char: %zu byte(s)\n", sizeof(char));
-	printf("Size of an int: %zu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
 	return (0);
 }
