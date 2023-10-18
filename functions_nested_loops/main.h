946f6e6f4b8bb629fca24bd1bcc6a4c2ee063297
bernardtchamou <tchamou.bernard@gmail.com>
2023-10-18 05:55:47 -0700

    header

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,4 +1,3 @@
-
 /**
 *File:main.h
 *Desc:fonctions contenu dans fichier en tete main.h
@@ -6,6 +5,13 @@
 
 void print_alphabet(void);
 void print_alphabet_x10(void);
+void jack_bauer(void);
+void times_table(void);
+void print_to_98(int n);
 int _putchar(char);
 int _islower(int c);
 int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+int add(int, int);
