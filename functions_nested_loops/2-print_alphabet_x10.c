dc008ba5c66b7154f131e4b9e1de1d0940545dd5
bernardtchamou <tchamou.bernard@gmail.com>
2023-10-18 01:23:04 -0700

    tache 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_alphabet_x10 - affriche l'alphabet 10 fois
+*
+* Return: 0 si code bon
+*/
+
+void print_alphabet_x10(void)
+{
+int a = 0;
+int b;
+while (a++ <= 9)
+{
+for (a = 'a'; a <= 'z'; a++)
+_putchar(b);
+}
+_putchar(10);
+}
