6202962c10412f2101a068ecda84b1c1afe7f503
bernardtchamou <tchamou.bernard@gmail.com>
2023-10-18 05:25:06 -0700

    tache 5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*print_sign - indique le signe d'un nombre
+*@n: argument n, correspondant au nombre à traiter
+*Return: 1 si c est une lettre 0 si autre
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0);
+	}
+		return (0);
+}
