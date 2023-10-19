384b3c03c323369965e8eeb8fa24087a2b33a47d
bernardtchamou <tchamou.bernard@gmail.com>
2023-10-19 04:49:21 -0700

    tache 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _add - ajoute un valer à un entier
+ * @num1:Premier numéro
+ * @num2:Deuxième numéro
+ * Return: somme de num1 et num2
+ */
+int add(int num1, int num2)
+{
+	int somme = num1 + num2;
+
+	return (somme);
+}
