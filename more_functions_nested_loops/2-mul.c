1c26008472edb1dad4db59d401bbcb13384926a5
bernardtchamou <tchamou.bernard@gmail.com>
2023-10-20 01:38:57 -0700

    tache 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*mul - function that multipliestwo numbers
+*@a: premier nombre
+*@b: second nombre
+*Return: somme
+*/
+
+int mul(int a, int b)
+{
+	int somme = a * b;
+
+	return (somme);
+}
