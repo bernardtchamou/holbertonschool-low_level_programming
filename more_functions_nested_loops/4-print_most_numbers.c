c6e0ecf3731eb05160438eb44d6ad33cf6df7bf3
bernardtchamou <tchamou.bernard@gmail.com>
2023-10-20 02:13:11 -0700

    tache 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*print_most_numbers - affiche de 0 à 9 sauf 2 et 4
+*Return: rien
+*/
+
+void print_most_numbers(void)
+{
+	int chiffre;
+
+	for (chiffre = 48; chiffre < 58; chiffre++)
+	{
+		if ((chiffre != 50) && (chiffre != 52))
+			{
+				_putchar(chiffre)
+			}
+	}
+_putchar('\n');
+}
