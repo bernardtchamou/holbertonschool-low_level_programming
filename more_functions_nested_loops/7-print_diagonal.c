48e71e9f6989cd3e584339355bbccdecfd1e2447
bernardtchamou <tchamou.bernard@gmail.com>
2023-10-20 07:13:12 -0700

    tache 7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -7,12 +7,16 @@
 
 void print_diagonal(int n)
 {
-	int i;
-	int j;
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
 
 	for (i = 1; i <= n; i++)
 	{
-		for(j = 1; j < i; j++)
+		for (j = 1; j <= i; j++)
 		{
 			_putchar(' ');
 		}
