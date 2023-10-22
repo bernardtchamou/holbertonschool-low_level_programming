2d0006bae4c22b0773450e09fb9b42e32d7b4243
bernardtchamou <tchamou.bernard@gmail.com>
2023-10-22 00:09:22 -0700

    tache 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - affiche les nombres de 1 à 100, fizz pour multiple de 3
+ * Buzz pour multiple de 5 et FizzBuzz pour les 2 multiple
+ * Return: 0 si code bon
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf(" Fizz");
+		} else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf(" Buzz");
+		} else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		} else if (i == 1)
+		{
+			printf("%d", i);
+		} else
+		{
+			printf(" %d", i);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
